bf3cd0d907af7029de330ecc2b78e47615fff976
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-04-19 00:09:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let Name_value=document.querySelectorAll("input")[0].value;
+	let Name_value1=document.querySelectorAll("input")[1].value;
+     // let Name_value2=document.querySelector("input")[2].value;
+	alert(Name_value +" "+ Name_value1);
 
 }
